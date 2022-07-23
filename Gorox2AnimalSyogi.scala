957720364418ff8a0971a3110e1a032e7a5d5011
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2022-07-23 17:09:40 +0900

    add some Koma class

--- Gorox2AnimalSyogi.scala
+++ Gorox2AnimalSyogi.scala
@@ -1,15 +1,63 @@
 class Gorox2AnimalSyogi(playFirst: Player, drawFirst: Player) {
-    private abstract class Koma {
+    import Koma._
 
+    private abstract class Koma {
+        val icon: Char
+        val side: Player
     }
 
-    private class LionKoma extends Koma {
-        
+    private object Koma {
+        def lion(side: Player): Koma = 
+            new LionKoma(side)
+
+        def dog(side: Player): Koma = 
+            new DogKoma(side)
+
+        def cat(side: Player): Koma = 
+            new CatKoma(side)
+
+        def chick(side: Player): Koma = 
+            new ChickKoma(side)
+
+        def chicken(side: Player): Koma = 
+            new ChickenKoma(side)
+
+        private class LionKoma(val side: Player) extends Koma {
+            val icon = 'ら'
+        }
+
+        private class DogKoma(val side: Player) extends Koma {
+            val icon = 'い'
+        }
+
+        private class CatKoma(val side: Player) extends Koma {
+            val icon = 'ね'
+        }
+
+        private class ChickKoma(val side: Player) extends Koma {
+            val icon = 'ひ'
+        }
+
+        private class ChickenKoma(val side: Player) extends Koma {
+            val icon = 'に'
+        }
     }
 
     private var nowPlayer = playFirst
-    private var mainField = Array.ofDim[Koma](5, 6)
-    private var subField = Array.ofDim[Koma](4, 4)
+    private var mainField: Array[Array[Koma]] = Array(
+        Array(cat(drawFirst), dog(drawFirst), lion(drawFirst), dog(drawFirst), cat(drawFirst)),
+        Array(null, null, null, null, null),
+        Array(null, chick(drawFirst), chick(drawFirst), chick(drawFirst), null),
+        Array(null, chick(playFirst), chick(playFirst), chick(playFirst), null),
+        Array(null, null, null, null, null),
+        Array(cat(playFirst), dog(playFirst), lion(playFirst), dog(playFirst), cat(playFirst))
+    )
+    private var subField: Array[Array[Koma]] = Array(
+        Array(null, null, null, null),
+        Array(null, null, null, null),
+        Array(null, null, null, null),
+        Array(null, null, null, null)
+    )
 }
 
 class Player(val name: String, val color: String)
@@ -17,5 +65,6 @@ class Player(val name: String, val color: String)
 object Main extends App {
     val playFirst = new Player("Penguin", "Green")
     val drawFirst = new Player("Tokage", "Blue")
+
     val g2as = new Gorox2AnimalSyogi(playFirst, drawFirst)
 }
